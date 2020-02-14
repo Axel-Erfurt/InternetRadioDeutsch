99d599f7cadcb5d3bbc83ee65796620601619ec1
Axel Schneider <axel99092@t-online.de>
2020-02-14 11:41:58 +0100

    Update InternetRadioDeutschInstallation.sh
    Installation angepasst, Icon wird nun in desktopfile übernommen

--- InternetRadioDeutschInstallation.sh
+++ InternetRadioDeutschInstallation.sh
@@ -2,36 +2,36 @@
 filename=$HOME/.local/share/InternetRadioDeutsch/myradio.txt
 if [ -e "$filename" ]
 then
-    echo "$filename found, copying to /tmp"
+    echo "$filename gefunden, kopiere nach /tmp"
     cp $filename /tmp
 else
-    echo "$filename not found"
+    echo "$filename nicht gefunden"
 fi
 sharedapps=$HOME/.local/share/applications/
 if [ -d "$sharedapps" ]
  then
-    echo "$sharedapps found"
+    echo "$sharedapps gefunden"
 else
-    echo "$sharedapps not found"
+    echo "$sharedapps nicht gefunden"
     mkdir $sharedapps
 fi
-echo "removing InternetRadioDeutsch"
+echo "lösche InternetRadioDeutsch"
 rm -rf ~/.local/share/InternetRadioDeutsch
 cd ~/.local/share/
-echo "downloading InternetRadioDeutsch ..."
+echo "herunterladen von InternetRadioDeutsch ..."
 wget https://github.com/Axel-Erfurt/InternetRadioDeutsch/archive/master.zip
-echo "unzip InternetRadioDeutsch"
-unzip -o master.zip
+echo "entpacke InternetRadioDeutsch"
+unzip -qq -o master.zip
 sleep 1
-echo "remove zip file"
+echo "lösche zip Datei"
 rm master.zip
 mv ~/.local/share/InternetRadioDeutsch-master ~/.local/share/InternetRadioDeutsch
 desktopfile=$HOME/.local/share/applications/InternetRadioDeutsch.desktop
 if [ -e "$desktopfile" ]
 then
-    echo "$desktopfile already exists"
+    echo "$desktopfile existiert schon"
 else
-    echo "$desktopfile not found"
+    echo "$desktopfile nicht gefunden, wird nun angelegt."
     cp $HOME/.local/share/InternetRadioDeutsch/InternetRadioDeutsch.desktop $HOME/.local/share/applications
 fi
 rf=/tmp/myradio.txt
@@ -40,14 +40,15 @@ then
     echo "restore myradio.txt"
     cp $rf $HOME/.local/share/InternetRadioDeutsch
 else
-    echo "$filename not found"
+    echo "$filename nicht gefunden"
 fi
+#cp ~/.local/share/InternetRadioDeutsch/InternetRadioDeutsch.desktop ~/.local/share/applications
 rm ~/Downloads/InternetRadioDeutschInstallation.sh
 cd ~/.local/share/applications/
 mv InternetRadioDeutsch.desktop InternetRadioDeutsch.desktop-bak
 icon=$(echo ~)/.local/share/InternetRadioDeutsch/radio_bg.png
 sed -e "s,Icon=.*,Icon=$icon,g" InternetRadioDeutsch.desktop-bak > InternetRadioDeutsch.desktop
 rm InternetRadioDeutsch.desktop-bak
-echo "starting InternetRadio ... please use tray icon context menu with right mouse button"
+echo "starte myRadio ... benutze Tray Kontextmenu mit rechter Maustaste"
 python3 ~/.local/share/InternetRadioDeutsch/myRadioD.py
 exit
